80650d71ad02a207bf70fa0dfa282715a84bd76e
harleenkaurchadha <harleen198.chadha@gmail.com>
2016-11-17 16:48:26 +0530

    Add files via upload

---
+++ Arduino files (hardware interface)/read_ESP.ino
@@ -0,0 +1,64 @@
+
+
+//This function goes and finds a keyword received from the ESP
+
+boolean read_ESP(const char keyword1[], int key_size, int timeout_val, byte mode){
+  timeout_start_val=millis();
+  char data_in[20];
+  int scratch_length=1;
+  key_size--;
+ 
+  for(byte i=0; i<key_size; i++){  
+           
+            while(!Serial2.available()){
+              if((millis()-timeout_start_val)>timeout_val){
+                Serial.println("timeout");
+                return 0;
+              }
+            }
+   
+    data_in[i]=Serial2.read();
+  
+    if(mode==1){//this will save all of the data to the data_from_ESP
+      data_from_ESP[scratch_length]=data_in[i];
+      data_from_ESP[0]=scratch_length;
+      scratch_length++;
+    }
+    
+  }
+
+  while(1){   //until keyword is found or timeout occurs
+
+          for(byte i=0; i<key_size; i++){
+       if(keyword1[i]!=data_in[i])
+       break;
+       if(i==(key_size-1)){
+       return 1; 
+       }
+     }
+     
+
+    //start rolling the buffer
+    for(byte i=0; i<(key_size-1); i++){
+      data_in[i]=data_in[i+1];
+      }
+ 
+         
+            while(!Serial2.available()){
+              if((millis()-timeout_start_val)>timeout_val){
+                Serial.println("timeout");
+                return 0;
+              }
+            }
+    
+    data_in[key_size-1]=Serial2.read();//save the new data in the last position in the buffer
+    
+      if(mode==1){
+      data_from_ESP[scratch_length]=data_in[key_size-1];
+      data_from_ESP[0]=scratch_length;
+      scratch_length++;
+    }
+    
+  }
+  
+}
---
+++ Arduino files (hardware interface)/setup_esp.ino
@@ -0,0 +1,69 @@
+boolean setup_ESP(){//returns a '1' if successful
+  delay(1000);
+  if(read_ESP(keyword_Ready,sizeof(keyword_Ready),5000,0))
+  Serial.println("MODULE STARTED");
+  Serial2.println("AT");// to make sure the ESP is responding
+   if(read_ESP(keyword_OK,sizeof(keyword_OK),1000,0))
+   { Serial.println("ESP CHECK OK");
+    delay(500);
+    }
+  else
+   { Serial.println("ESP CHECK FAILED");
+  serial_dump_ESP();
+   }
+  Serial2.println("AT+CWQAP");
+//  if(read_ESP(keyword_OK,sizeof(keyword_OK),2000,0))
+//  Serial.println("WIFI DISCONNECTED");
+//  delay(500);
+
+   Serial2.print("AT+CWMODE=");// set the CWMODE
+   Serial2.println(CWMODE);
+   if(read_ESP(keyword_OK,sizeof(keyword_OK),2000,0))
+ {  Serial.println("ESP CWMODE SET");
+     delay(500);
+ }  
+  else
+  {
+    Serial.println("ESP CWMODE SET FAILED");
+   serial_dump_ESP();  
+  }
+   
+   String cmd="AT+CWJAP=\"";
+       cmd+=SSID_ESP;
+       cmd+="\",\"";
+       cmd+=SSID_PASS;
+       cmd+="\"";
+Serial2.println(cmd);
+       Serial.println(cmd);
+   if(read_ESP(keyword_co,sizeof(keyword_co),50000,0))
+    {Serial.println("WIFI CONNECTED");
+    delay(1000);}
+  else
+    {Serial.println("ESP SSID SET FAILED");   
+  serial_dump_ESP();
+    }
+    Serial2.print("AT+CIPMODE=0");// set the CIPMODE
+    if(read_ESP(keyword_OK,sizeof(keyword_OK),5000,0))
+      {
+        Serial.println("ESP SET TO NORMAL MODE");
+      delay(500);
+      }
+      else
+      {
+        Serial.println("CIPMODE=0 NOT EXECUTED");
+        serial_dump_ESP();
+      }
+   Serial2.print("AT+CIPMUX=");// set the CIPMUX
+   Serial2.println(CIPMUX);
+   if(read_ESP(keyword_OK,sizeof(keyword_OK),5000,0))//go look for keyword "OK" or "no change
+   { Serial.println("ESP CIPMUX SET");
+   delay(500);
+    }
+  else
+    {Serial.println("ESP CIPMUX SET FAILED"); 
+  serial_dump_ESP();
+ 
+    }
+ 
+  
+}
